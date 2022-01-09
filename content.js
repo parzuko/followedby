14fba4895ed6f8c905a414b7c11c73dbb0c23b9a
Jivansh Sharma <jivansh17@gmail.com>
2022-01-09 20:00:28 +0530

    chore: get follower and following list

--- content.js
+++ content.js
@@ -1,2 +1,25 @@
+const clientName = document.querySelector('meta[name="user-login"]').content;
 const currentUser = window.location.pathname.slice(1);
-console.log(currentUser);
+
+let clientFollowing = [];
+let userFollowers = [];
+
+fetch(`https://api.github.com/users/${clientName}/following?per_page=100`)
+    .then((response) => response.json())
+    .then((data) => {
+        clientFollowing = [...data];
+    })
+    .catch((error) => console.error(error));
+
+fetch(`https://api.github.com/users/${currentUser}/followers?per_page=100`)
+    .then((response) => response.json())
+    .then((data) => {
+        userFollowers = [...data];
+        const filteredArray = clientFollowing.filter((value) =>
+            userFollowers.includes(value)
+        );
+    })
+    .catch((error) => console.error(error));
+
+// console.log(clientFollowing, userFollowers);
+// console.log(clientFollowing);
