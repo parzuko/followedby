92590c66b053c0d7e957ff76a9ebf42b482447de
Jivansh Sharma <jivansh17@gmail.com>
2022-01-10 17:00:25 +0530

    feat: create getFollower and getFollowing function

--- content.js
+++ content.js
@@ -3,6 +3,20 @@ const currentUser = window.location.pathname.slice(1);
 
 const followCountTag = document.querySelector(".vcard-details");
 
+const getUserFollowing = async (userName, pageNumber) => {
+    const response = await fetch(
+        `https://api.github.com/users/${userName}/following?per_page=100?page=${pageNumber}`
+    );
+    return await response.json();
+};
+
+const getUserFollowers = async (userName, pageNumber) => {
+    const response = await fetch(
+        `https://api.github.com/users/${userName}/followers?per_page=100?page=${pageNumber}`
+    );
+    return await response.json();
+};
+
 const tag = document.createElement("p");
 const text = document.createTextNode("No followers in common");
 tag.appendChild(text);
