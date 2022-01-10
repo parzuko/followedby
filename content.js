cef769e21d53f05567418bc20bbc9c7a5c7ff3cd
Jivansh Sharma <jivansh17@gmail.com>
2022-01-10 17:01:11 +0530

    chore: find overlap and breaking request for pageNumber

--- content.js
+++ content.js
@@ -17,6 +17,38 @@ const getUserFollowers = async (userName, pageNumber) => {
     return await response.json();
 };
 
+const getMultiPageResponse = async (getRequest, userName) => {
+    let list = null,
+        page = 1,
+        results = [];
+    do {
+        list = await getRequest(userName, page++);
+        console.log(list);
+        results = results.concat(list);
+    } while (list.length > 1);
+
+    return results;
+};
+
+const getOverlap = async () => {
+    const followers = await getMultiPageResponse(getUserFollowing, clientName);
+    const following = await getMultiPageResponse(getUserFollowers, currentUser);
+
+    console.log(followers, following);
+
+    const filtered = [];
+
+    for (const followee of following) {
+        for (const follower of followers) {
+            if (followee.login === follower.login) {
+                filtered.push(followee);
+                break;
+            }
+        }
+    }
+    return filtered;
+};
+
 const tag = document.createElement("p");
 const text = document.createTextNode("No followers in common");
 tag.appendChild(text);
