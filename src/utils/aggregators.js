594115c69138d2c3abb7705f558e385d2db98d56
Jivansh Sharma <jivansh17@gmail.com>
2022-01-30 19:02:08 +0530

    chore: export aggregator funcs

---
+++ src/utils/aggregators.js
@@ -0,0 +1,37 @@
+import { getOrSet } from "./storage.js";
+
+const getMultiPageResponse = async (getRequest, userName) => {
+    let page = 1;
+    const results = [];
+
+    while (true) {
+        const nextPage = await getRequest(userName, page++);
+        if (nextPage.length === 100) {
+            results.push(...nextPage);
+            continue;
+        } else if (nextPage.length < 100 && nextPage.length > 1) {
+            results.push(...nextPage);
+            break;
+        }
+        break;
+    }
+    return results;
+};
+
+export const getOverlap = async (clientName, currentUser) => {
+    if (clientName === currentUser) return;
+    const followers = await getOrSet(getMultiPageResponse, clientName, false);
+    const following = await getOrSet(getMultiPageResponse, currentUser, true);
+
+    const filtered = [];
+
+    for (const followee of following) {
+        for (const follower of followers) {
+            if (followee.login === follower.login) {
+                filtered.push(followee);
+                break;
+            }
+        }
+    }
+    return filtered;
+};
