5188516e9fce062703549b41107e82a033217f00
parzuko <jivansh17@gmail.com>
2022-07-09 23:28:41 +0530

    :alien: Fetch user details

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,10 @@
+/**
+ * Returns user following list from GitHub API
+ *
+ * @param {string} username userName to fetch details for
+ * @param {number} pageNumber pageNumber for paginated requests
+ * @return {Array<object>} User's following array
+ */
 export const getUserFollowing = async (userName, pageNumber) => {
     const response = await fetch(
         `https://api.github.com/users/${userName}/following?per_page=100&page=${pageNumber}`
@@ -5,6 +12,13 @@ export const getUserFollowing = async (userName, pageNumber) => {
     return await response.json();
 };
 
+/**
+ * Returns user followers list from GitHub API
+ *
+ * @param {string} username userName to fetch details for
+ * @param {number} pageNumber pageNumber for paginated requests
+ * @return {Array<object>} User's followers array
+ */
 export const getUserFollowers = async (userName, pageNumber) => {
     const response = await fetch(
         `https://api.github.com/users/${userName}/followers?per_page=100&page=${pageNumber}`
