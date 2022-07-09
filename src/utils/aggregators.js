c5302607719b372c3081f75b60f86ae76f7e00a7
parzuko <jivansh17@gmail.com>
2022-07-10 03:02:52 +0530

    :recycle: Update aggregators

--- src/utils/aggregators.js
+++ src/utils/aggregators.js
@@ -1,5 +1,12 @@
 import { getOrSet } from "./storage.js";
 
+/**
+ * Returns user followers/following list from all pages of GitHub API
+ *
+ * @param {getRequest} request to await
+ * @param {userName} userName to fetch details for
+ * @return {Array<object>} User's followers/following array
+ */
 const getMultiPageResponse = async (getRequest, userName) => {
     let page = 1;
     const results = [];
@@ -18,11 +25,18 @@ const getMultiPageResponse = async (getRequest, userName) => {
     return results;
 };
 
+/**
+ * Returns mutual followers for current user 
+ *
+ * @param {string} clientName logged in user to fetch details for
+ * @param {string} currentUser current page's user to fetch details for
+ * @return {Array<object>} Client's followers which are in current user's following
+ */
 export const getOverlap = async (clientName, currentUser) => {
-    if (clientName === currentUser) return null;
+    if (clientName === currentUser) return [];
     const followers = await getOrSet(getMultiPageResponse, clientName, false);
     const following = await getOrSet(getMultiPageResponse, currentUser, true);
-    const OVERFLOW_COUNT = 2;
+    const OVERFLOW_COUNT = 13;
 
     const filtered = [];
 
